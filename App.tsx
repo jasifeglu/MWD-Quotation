2d1dc2a5e644fc331463ca8ab6ea3fa4f5608d28
jasifeglu <jasif.eglu@gmail.com>
2025-03-09 16:48:58 +0530

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <h1 className="text-3xl font-bold text-center p-8">
+        Welcome to the Quotation App
+      </h1>
+    </div>
+  );
+}
+
+export default App;
